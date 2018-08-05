8737bfe06d04933ff580fdd0b8c684aa3e285702
Marcelo Oikawa <marcelo.oikawa@olxbr.com>
2018-08-05 15:23:37 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const Kickflip = require("./kickflip")
+
+let kickflip = new Kickflip(15, 2)
+console.info(kickflip.factor("BUMP:2020:21"))
+kickflip.inc("BUMP:2020:21")
+
+console.info(kickflip.factor("BUMP:2020:21"))
+kickflip.inc("BUMP:2020:21")
+
+console.info(kickflip.factor("BUMP:2020:21"))
+kickflip.inc("BUMP:2020:21")
+
+console.info(kickflip.factor("BUMP:2020:21"))
+kickflip.inc("BUMP:2020:21")
+
+console.info(kickflip.factor("BUMP:2020:21"))
+kickflip.inc("BUMP:2020:21")
+
+console.info(kickflip.factor("BUMP:2020:21"))
+kickflip.inc("BUMP:2020:21")
---
+++ kickflip.js
@@ -0,0 +1,29 @@
+const NodeCache = require( "node-cache" );
+
+class Kickflip {
+
+    constructor(ttl_sec = 10, limit = 10) {
+        this.ttl_sec = ttl_sec
+        this.limit = limit
+        this.cache = new NodeCache()
+      }
+
+      inc(key) {
+        let count = this.cache.get(key) || 0
+        this.cache.set(key, count + 1, this.ttl_sec)
+      }
+
+      get(key) {
+        return this.cache.get(key)
+      }
+
+      factor(key) {
+        let count = this.cache.get(key)
+        if (count == undefined || count <= this.limit) {
+          return 1;
+        }
+        return Math.round(count / this.limit)
+      }
+}
+
+module.exports = Kickflip
