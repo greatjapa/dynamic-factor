e8f9fd9d21d0702e5a88dfebb2e922a595b53902
Marcelo Oikawa <marcelo.oikawa@olxbr.com>
2018-09-01 22:13:43 -0300

    adding more unit tests

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const NodeCache = require( "node-cache" );
 
 class Dynamic {
 
-    constructor(ttl_sec = 10, limit = 10) {
+    constructor(ttl_sec = 10, denominator = 10) {
       if (!Number.isInteger(ttl_sec)) {
         throw Error("ttl_sec should be integer")
       }
-      if (!Number.isInteger(limit)) {
-        throw Error("limit should be integer")
+      if (!Number.isInteger(denominator)) {
+        throw Error("denominator should be integer")
       }
       this.ttl_sec = ttl_sec
-      this.limit = limit
+      this.denominator = denominator
       this.cache = new NodeCache()
     }
 
@@ -30,11 +30,11 @@ class Dynamic {
     }
 
     factor(key) {
-      let count = this.cache.get(key)
-      if (count == undefined || count <= this.limit) {
-        return 1;
+      let value = this.cache.get(key)
+      if (value == undefined) {
+        return 0;
       }
-      return count / this.limit
+      return value / this.denominator
     }
 }
 
